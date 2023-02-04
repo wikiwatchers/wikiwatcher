862c0dcba71f29cef0ae8139a54dd05577071324
Amelia Miner <ameliamminer@gmail.com>
2023-02-04 11:41:24 -0800

    Addressing linter errors

--- API/app.py
+++ API/app.py
@@ -1,3 +1,7 @@
+""" API/App.py
+Defines endpoints of the API
+temporary/early version for setting up CI/CD
+"""
 import requests as r
 from flask import Flask
 app = Flask("WikiWatcher")
@@ -5,15 +9,22 @@ app = Flask("WikiWatcher")
 
 @app.route("/")
 def index():
+    """Temporary test code - may put a UI here at some point?"""
     return "Test Flask response"
 
 
 @app.route("/revisionHistory/<title>")
 def get_revisions(title):
-    # This will be abstracted away into a RevisionHistory class
+    """
+    Returns a JSON object containing the 20 most recent 
+    revision objects to the article specified by <title>
+
+    This is exploratory/prototype code which will likely be
+    abstracted away into a RevisionHistory class.
+    """
     url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/"\
         + "<title>/history".replace("<title>", title)
-    revisions = r.get(url=url)
+    revisions = r.get(url=url, timeout=5)
     # print(json.dumps(revisions.json(), indent=1))
     return revisions.json()["revisions"]
 
