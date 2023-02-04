4563ffa97c97e52f8373bd1374a86c4c30fc2719
Amelia Miner <ameliamminer@gmail.com>
2023-02-04 11:15:35 -0800

    Updated requirements, addressed a linting issue, Pylint action setup

--- API/app.py
+++ API/app.py
@@ -9,7 +9,8 @@ def index():
 @app.route("/revisionHistory/<title>")
 def get_revisions(title):
 	# This will be abstracted away into a RevisionHistory class
-	url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/<title>/history".replace("<title>", title)
+	url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/"\
+			+ "<title>/history".replace("<title>", title)
 	revisions = r.get(url=url)
 	#print(json.dumps(revisions.json(), indent=1))
 	return revisions.json()["revisions"]
