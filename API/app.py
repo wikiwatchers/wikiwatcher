c58d4d7363c71919bb1f53b3f9ea1594b957f8f9
Amelia Miner <ameliamminer@gmail.com>
2023-02-04 11:35:01 -0800

    formatted code with autopep8, set up pylintrc

--- API/app.py
+++ API/app.py
@@ -2,18 +2,21 @@ import requests as r
 from flask import Flask
 app = Flask("WikiWatcher")
 
+
 @app.route("/")
 def index():
-	return "Test Flask response"
+    return "Test Flask response"
+
 
 @app.route("/revisionHistory/<title>")
 def get_revisions(title):
-	# This will be abstracted away into a RevisionHistory class
-	url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/"\
-			+ "<title>/history".replace("<title>", title)
-	revisions = r.get(url=url)
-	#print(json.dumps(revisions.json(), indent=1))
-	return revisions.json()["revisions"]
+    # This will be abstracted away into a RevisionHistory class
+    url = "https://api.wikimedia.org/core/v1/wikipedia/en/page/"\
+        + "<title>/history".replace("<title>", title)
+    revisions = r.get(url=url)
+    # print(json.dumps(revisions.json(), indent=1))
+    return revisions.json()["revisions"]
+
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
