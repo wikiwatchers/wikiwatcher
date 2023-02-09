27deae9a6d0f0c53d4d5492619264d8502753066
Nora Luna <86637985+per-sephone@users.noreply.github.com>
2023-02-08 22:04:23 -0800

    get_contents & assign_contents methods written

--- src/revision.py
+++ src/revision.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+import requests
+import json
 
 
 class User():
@@ -12,27 +14,69 @@ class Revision():
         # possible params
         self.json: dict = None
         self.id: int = None
+        self.title: str = None
         self.timestamp: datetime = None
         self.pageId: int = None
         self.user: User = None
         self.minor: bool = None
         self.tags: list[str] = None
-
-        # present in Usercontribs but not Pagehistory
+        self.comment: str = None
         self.parentId: int = None
         # present in Pagehistory but not Usercontribs
         self.size: int = None
-        # TODO
 
-    def get_contents(self):
+    def assign_contents(self, pages):
+
+        revisions = pages["revisions"][0]
+
+        self.json = pages
+        self.id = revisions["revid"]
+        self.title = pages["title"]
+        self.timestamp = revisions["timestamp"]
+        self.pageId = pages["pageid"]
+        self.user = User(revisions["user"], revisions["userid"])
+        self.minor = revisions["minor"]
+        self.tags = revisions["tags"]
+        self.parentId = revisions["parentid"]
+        self.size = revisions["size"]
+        self.comment = revisions["comment"]
+
+
+    def get_contents(self, title):
         """ Returns the content of the page at this revision
         Hits mediawiki.org API with
         "action": "query",
         "prop": "revisions",
         "rvprop": "content"
         """
-        # TODO
-        pass
+
+        S = requests.Session()
+
+        URL = "https://www.wikipedia.org/w/api.php"
+
+        PARAMS = {
+            "action": "query",
+            "prop": "revisions",
+            "titles": title,
+            "rvprop": "timestamp|user|userid|comment|content|tags|ids|size|flags",
+            "rvslots": "main",
+            "formatversion": "2",
+            "format": "json",
+        }
+
+        R = S.get(url=URL, params=PARAMS)
+        DATA = R.json()
+
+        PAGES = DATA["query"]["pages"]
+
+        #assign contents of PAGES to the object
+        self.assign_contents(PAGES[0])
+
+        #print(json.dumps(PAGES, indent=1))
+
+
+
+
 
     def get_diff(self, toId: int = None):
         """ Returns the difference between this revision and its parent 
