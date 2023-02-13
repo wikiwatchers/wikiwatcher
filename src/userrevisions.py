5838bcbb7f02378b38248760d602dbe7189e6d04
golson-pdx <golson@pdx.edu>
2023-02-13 12:15:45 -0800

    Redo branch init - user revisions class initially built

---
+++ src/userrevisions.py
@@ -0,0 +1,34 @@
+'''defines user revisions class'''
+from datetime import datetime
+from revision import Revision, User
+import requests
+
+class UserRevisions():
+    '''User revision object holds a list of user's revisions'''
+    def __init__(self) -> None:
+        # possible params
+        self.json: dict = None
+        self.user: User = None
+        self.revisions: list[Revision] = None
+
+def get_contents(self): 
+        ''' notes '''
+
+        session = requests.Session()
+
+        url = "https://www.wikipedia.org/w/api.php"
+
+        params = {
+            "action": "query",
+            "format": "json",
+            "list": "usercontribs",
+            "formatversion": "2",
+            #ucuser - placeholder
+             "ucuser": "Jimbo%20Wales" 
+        }
+        try:
+            request = session.get(url=url, params=params)
+        except Exception as exc:
+            raise SystemExit("User Name missing") from exc
+        data = request["query"].json()
+        print(data)
