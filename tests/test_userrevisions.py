ffca36d6a38b34532e3777999be511429f97c636
golson-pdx <golson@pdx.edu>
2023-02-14 13:27:24 -0800

    Pylint updates for tests

--- tests/test_userrevisions.py
+++ tests/test_userrevisions.py
@@ -2,15 +2,14 @@
 import json
 import requests
 from userrevisions import UserRevisions,  URL
-with open('user_test.json', 'r', encoding='utf-8') as q:
-    TPARAMS = json.load(q)
 
 T_UREVS = None
 
 def test_userrevision_init():
     '''Tests userrevision init'''
-    global T_UREVS, TPARAMS
-    j = requests.get(URL, TPARAMS).json()
+    with open('user_test.json', 'r', encoding='utf-8') as q:
+        test_params = json.load(q)
+    j = requests.get(URL, test_params).json()
     userrevjson = j["query"]["usercontribs"]
     T_UREVS = UserRevisions(userrevjson)
     assert T_UREVS.revisions[0].user == 'Jimbo Wales'
