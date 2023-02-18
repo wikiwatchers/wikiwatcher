056bb344569f713aaefa48d219e32d6145d351ad
Amelia Miner <amminer@pdx.edu>
2023-02-18 13:25:11 -0800

    mostly (?) done with rough core flask endpoints, Wrote timestamp function for eventual history base class (still need to adjust  file structure..?)

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ import sys
 from flask import Flask, request
 from markdown import markdown
 from src.revision import URL
-from src.userrevisions import UserRevisions
+from src.userhistory import UserHistory
+from src.articlehistory import ArticleHistory
 try:
-    from src.articlerevisions import ArticleRevisions
+    from src.articlehistory import ArticleRevisions
 except ModuleNotFoundError as modError:
     print(modError, "Waiting to merge")
 #import src.articlerevisions
@@ -33,30 +34,68 @@ def index():
         ret = markdown(readme.read())
     return ret
 
-@app.route("/articleRevisions/<title>")
-def get_article_revisions(title):
-    """ /revisionHistory/<title>?... """
+@app.route("/articleHistory/<title>")
+def get_article_history(title):
+    """ /articleHistory/<title>?... """
     # gather user inputs
     tags: list[str] = parse_tags(request.args.get("tags", default=None, type=str))
     keyword: str = request.args.get("keyword", default=None, type=str)
-    user: str = request.args.get("user", default=None, type=str)
+    rvuser: str = request.args.get("user", default=None, type=str)
+    startyear: int = request.args.get("startyear", default=None, type=int)
+    startmonth: int = request.args.get("startmonth", default=None, type=int)
+    startday: int = request.args.get("startday", default=None, type=int)
+    starthour: int = request.args.get("starthour", default=None, type=int)
+    startminute: int = request.args.get("startminute", default=None, type=int)
+    startsecond: int = request.args.get("startsecond", default=None, type=int)
+    endyear: int = request.args.get("endyear", default=None, type=int)
+    endmonth: int = request.args.get("endmonth", default=None, type=int)
+    endday: int = request.args.get("endday", default=None, type=int)
+    endhour: int = request.args.get("endhour", default=None, type=int)
+    endminute: int = request.args.get("endminute", default=None, type=int)
+    endsecond: int = request.args.get("endsecond", default=None, type=int)
     # gather and filter revisions
-    if "src.articlerevisions" in sys.modules:
+    if "src.articlehistory" in sys.modules:
         revisions = ArticleRevisions(titles=title,
                                      startyear=startyear, startmonth=startmonth, startday=startday,
                                      starthour=starthour, startminute=startminute, startsecond=startsecond,
                                      endyear=endyear, endmonth=endmonth, endday=endday,
                                      endhour=endhour, endminute=endminute, endsecond=endsecond,
-                                     tags=tags, user=user, keyword=keyword)
+                                     tags=tags, rvuser=rvuser, keyword=keyword)
     else:
         ret = "-1" # placeholder - we should discuss what to do in this case?
 
     return ret
 
-@app.route("/userRevisions/<username>")
+@app.route("/userHistory/<username>")
 def get_user_revisions(username):
-    """ /revisionHistory/<username>?fromdate=<>&todate=<>&keyword=<>&tags=<>&keyword=<> """
-    return username # to do
+    """ /userHistory/<username>?... """
+    # gather user inputs
+    tags: list[str] = parse_tags(request.args.get("tags", default=None, type=str))
+    keyword: str = request.args.get("keyword", default=None, type=str)
+    title: str = request.args.get("user", default=None, type=str)
+    startyear: int = request.args.get("startyear", default=None, type=int)
+    startmonth: int = request.args.get("startmonth", default=None, type=int)
+    startday: int = request.args.get("startday", default=None, type=int)
+    starthour: int = request.args.get("starthour", default=None, type=int)
+    startminute: int = request.args.get("startminute", default=None, type=int)
+    startsecond: int = request.args.get("startsecond", default=None, type=int)
+    endyear: int = request.args.get("endyear", default=None, type=int)
+    endmonth: int = request.args.get("endmonth", default=None, type=int)
+    endday: int = request.args.get("endday", default=None, type=int)
+    endhour: int = request.args.get("endhour", default=None, type=int)
+    endminute: int = request.args.get("endminute", default=None, type=int)
+    endsecond: int = request.args.get("endsecond", default=None, type=int)
+    # gather and filter revisions
+    if "src.userhistory" in sys.modules:
+        revisions = UserHistory(user=username,
+                                startyear=startyear, startmonth=startmonth, startday=startday,
+                                starthour=starthour, startminute=startminute, startsecond=startsecond,
+                                endyear=endyear, endmonth=endmonth, endday=endday,
+                                endhour=endhour, endminute=endminute, endsecond=endsecond,
+                                tags=tags, rvuser=username, keyword=keyword)
+    else:
+        ret = "-1" # placeholder - we should discuss what to do in this case?
+    return ret
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/articlehistory.py
+++ src/articlehistory.py
@@ -1,3 +1,5 @@
+from src.exceptions import BadRequestException
+
 class ArticleRevisions:
 
     def __init__(self, titles) -> None:
@@ -8,15 +10,10 @@ class ArticleRevisions:
         if year:
             ret = str(year)
         else:
-            raise Exception #TODO
+            raise BadRequestException("invalid date/time specification")
         ret += str(month).rjust(2, "0") if month else "01"
         ret += str(day).rjust(2, "0") if day else "01"
         ret += str(hour).rjust(2, "0") if hour else "00"
         ret += str(minute).rjust(2, "0") if minute else "00"
         ret += str(second).rjust(2, "0") if second else "01"
         return ret
-
-if __name__ == "__main__":
-    a = ArticleRevisions()
-    print('20080607040130')
-    print(a.format_timestamp(2008, 6, 7, 4, 1, 30))
