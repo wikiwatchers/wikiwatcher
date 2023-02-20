6b3a18df9142c1fbcf31ce1f74518449ea2b130f
Nora Luna <86637985+per-sephone@users.noreply.github.com>
2023-02-19 17:54:52 -0800

    test for article history

--- tests/test_article_history.py
+++ tests/test_article_history.py
@@ -2,4 +2,9 @@ import pytest
 from history import History
 from article_history import ArticleHistory
 
-art = article_history("Techno")
+def test___init__():
+    art = ArticleHistory("Techno", "Rio65trio")
+    assert art.titles == "Techno"
+    assert art.user == "Rio65trio"
+    assert len(art.revisions) == 10
+    assert art.pageid == 23958411
