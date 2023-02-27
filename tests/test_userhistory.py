8e6709c573374d359d7005a4f3b2824623429c26
golson-pdx <golson@pdx.edu>
2023-02-26 21:15:31 -0800

    update user history tests

--- tests/test_userhistory.py
+++ tests/test_userhistory.py
@@ -7,24 +7,22 @@ from userhistory import UserHistory
 
 def test_userhistory_init():
     '''Tests user history init'''
-    user_contribs = UserHistory("Jimbo Wales")
+    user_history = UserHistory("Jimbo Wales")
 
-    assert user_contribs.revisions[0]['user'] == 'Jimbo Wales'
-    assert user_contribs.revisions[0]['userid'] == 24
-    assert len(user_contribs.revisions) == 10
+    assert user_history.user == "Jimbo Wales"
 
-    another_user_contribs = UserHistory("RobinHood70")
+def test_userhistory_tag_filter():
+    '''Tests user history filtering'''
+    user_history = UserHistory("Jimbo Wales", tags=["discussiontools-newtopic"])
 
-    assert another_user_contribs.revisions[0]['user'] == 'RobinHood70'
-    assert another_user_contribs.revisions[0]['userid'] == 4859457
-    assert len(another_user_contribs.revisions) == 10
+    assert user_history.user == "Jimbo Wales"
+    assert len(user_history.tags) == 1
+    assert user_history.tags[0] == "discussiontools-newtopic"
 
-def test_userhistory_keyword():
-    '''Tests user history with keyword - TO DO after filtering'''
-    keyword = "Talk:Archives of Venice"
+def test_userhistory_keyword_filter():
+    '''Tests user history filtering'''
+    user_history = UserHistory("Jimbo Wales", keyword="Duplicate page")
 
-    user_contribs = UserHistory('Jimbo Wales', keyword)
+    assert user_history.user == "Jimbo Wales"
+    assert user_history.keyword == "Duplicate page"
 
-    print(user_contribs.revisions[8])
-
-    assert user_contribs.revisions[8]['title'] == keyword
