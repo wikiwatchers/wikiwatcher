038daf24d43fec6d0566d417aec0ef9889ea1fec
Nora Luna <86637985+per-sephone@users.noreply.github.com>
2023-03-07 21:25:24 -0800

    mostly working; prints out datetime graph

--- src/histogram.py
+++ src/histogram.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import numpy as np
 from plot import Plot
 from articlehistory import ArticleHistory
@@ -8,38 +9,32 @@ import time
 class Histogram(Plot):
     def __init__(self, history):
         super().__init__(history)
-        
-        unix_time_list = self.datetime_to_unixtime()
-        self.set_x_axis(unix_time_list)
 
+        self.x_axis = self.pull_dates_from_history()
         self.plot_graph()
 
-        print(self.x_axis)
-        print(type(self.x_axis))
+        #print(self.x_axis)
 
-    def datetime_to_unixtime(self):
-        unix_time_list = []
+    def pull_dates_from_history(self):
+        datetime_list = []
         for each_rev in self.history.revisions:
-            unix_time_list.append((time.mktime(each_rev.timestamp_to_datetime().timetuple())))
-        return unix_time_list
-
-    def set_x_axis(self, unix_time_list):
-        self.x_axis = np.array(unix_time_list)
+            this_datetime = each_rev.timestamp_to_datetime()
+            datetime_list.append(mdates.date2num(this_datetime))
+        return np.array(datetime_list)
     
     def plot_graph(self):
-        plt.style.use(self.style)
 
         fig, ax = plt.subplots()
 
-        ax.hist(self.x_axis, bins=8, linewidth=0.5, edgecolor="white")
+        ax.hist(self.x_axis, bins=50, color="lightblue", edgecolor="black")
 
-        ax.set(xlim=(self.x_axis[0], self.x_axis[(len(self.x_axis)-1)]), 
-               xticks=np.linspace(self.x_axis[0], self.x_axis[(len(self.x_axis)-1)], 8),
-               ylim=(0, 15), yticks=np.linspace(0, 15, 3))
-        
-        plt.xticks(rotation=45, ha='right')
+        locator = mdates.AutoDateLocator()
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(mdates.AutoDateFormatter(locator))
+        plt.xticks(rotation=45)
 
         plt.show()
+        
 
 
 if __name__=="__main__":
--- src/plot.py
+++ src/plot.py
@@ -7,7 +7,7 @@ class Plot:
     """Plot base class contains basic plot information"""
     def __init__(self, history):
         self.history: History = history
-        self.style: str = "seaborn-darkgrid" #default?
+        #self.style: str = "seaborn-darkgrid" #default?
         # _mpl-gallery = histogram
         # _mpl-gallery-nogrid = pie chart
         self.x_axis = []
