6b2e8669be6a8ad3f73a4f0ff5c098c3f252cffd
Amelia Miner <amminer@pdx.edu>
2023-03-11 15:46:35 -0800

    fixed some test issues to fit refactor, fixed some strange way in which I broke Plot

--- src/plot.py
+++ src/plot.py
@@ -1,27 +1,27 @@
 """Base class for Plot object"""
 try:
-    from src.exceptions import NoRevisionsException
     from src.history import History
     from src.revision import Revision
 except ModuleNotFoundError:
-    from exceptions import NoRevisionsException
     from history import History
     from revision import Revision
+
 
 class Plot:
     """Plot base class contains basic plot information"""
     def __init__(self, history):
-        if len(history.revisions) == 0:
-            raise NoRevisionsException("No revisions matching filter parameters")
         self.history: History = history
-        #self.style: str = "seaborn-darkgrid" #default?
-        # _mpl-gallery = histogram
-        # _mpl-gallery-nogrid = pie chart
+        self.graph = None
         self.x_axis = []
+        self.x_axis_label: str = None
+        self.y_axis_label: str = None
         self.title: str = None
 
-    def set_x_axis(self, revisions_list: list[Revision],
+    def set_x_axis_data(self, revisions_list: list[Revision],
                    revision_property: str):
         """set x graphing data to any parameter in revision object"""
-        for each_rev in revisions_list:
-            self.x_axis.append(vars(each_rev)[revision_property])
+        try:
+            for each_rev in revisions_list:
+                self.x_axis.append(vars(each_rev)[revision_property])
+        except KeyError:
+            print("Revisions do not contain this key")
